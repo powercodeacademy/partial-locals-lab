5be735b83eed612cb3564a7adf8f6d5191e29c0f
Mike <mclancy96@gmail.com>
2025-08-22 15:49:03 -0400

    Fix spec to allow for students/student or just student path

--- spec/views/student_show_spec.rb
+++ spec/views/student_show_spec.rb
@@ -1,23 +1,27 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "show_view" do
-  let(:student){ Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago))}
+RSpec.describe 'show_view' do
+  before do
+    lookup_context.prefixes << 'students'
+  end
+  let(:student) { Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago)) }
 
-  it "renders student information from the classroom show view" do
+  it 'renders student information from the classroom show view' do
     assign(:student, student)
-    render :template => "students/show"
+    render template: 'students/show'
     expect(rendered).to include('Bobby')
   end
 
-  it "renders a students/student partial" do
+  it 'renders a students/student partial' do
     assign(:student, student)
-    render :template => "students/show"
-    expect(rendered).to render_template(:partial => "students/student", locals: {student: student})
+    render template: 'students/show'
+    expect(rendered).to render_template(partial: 'students/_student')
+      .or render_template(partial: '_student')
   end
 
-  it "displays the student information from the partial" do
+  it 'displays the student information from the partial' do
     assign(:student, student)
-    render :partial => "students/student", locals: {student: student}
-    expect(rendered).to match /Bobby/
+    render partial: 'students/student', locals: { student: student }
+    expect(rendered).to match(/Bobby/)
   end
 end
